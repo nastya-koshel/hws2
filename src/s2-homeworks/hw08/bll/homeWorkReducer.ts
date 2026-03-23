881709a5e9c34f777a31801a08c827ea87f710e7
Nastya Koshel <nastyakoshel68@gmail.com>
2026-03-23 22:19:57 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,13 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: any): any => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            return action.payload === 'up' ? state.sort( (a,b) => a.age - b.age  ) : state.sort( (a,b) => b.age - a.age)
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((u) => u.age > action.payload) // need to fix
         }
         default:
             return state
