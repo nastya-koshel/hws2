31d634422522b39e8d48c55513c6182328cff3a7
Nastya Koshel <nastyakoshel68@gmail.com>
2026-03-25 15:01:42 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,13 +8,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
     switch (action.type) {
         case 'sort': {
             if (action.payload === 'up') {
-                return state.sort( (a, b) => a.name > b.name ? 1 : -1 );
+                return [...state].sort( (a, b) => a.name > b.name ? 1 : -1 );
             } else if (action.payload === 'down') {
-                return state.sort( (a, b) => a.name < b.name ? 1 : -1 );
+                return [...state].sort( (a, b) => a.name < b.name ? 1 : -1 );
             } else {
                 return state
             }
-
         }
         case 'check': {
             return state.filter( (p) => p.age >= action.payload)
