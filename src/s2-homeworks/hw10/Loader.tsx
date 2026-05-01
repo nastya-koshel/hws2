3f3bb255aed955d4838ae68f6c69aeeb061b6389
Nastya Koshel <nastyakoshel68@gmail.com>
2026-05-01 16:07:30 +0300

    HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,5 +1,6 @@
 import s from './Loader.module.css'
+import {CircularProgress} from "@mui/material";
 
-
-export const Loader = () => <div className={s.loader}>Загрузка
+export const Loader = () => <div className={s.loader}>
+    <CircularProgress sx={{color: "#0066CC"}} size="90px" aria-label="Loading…" />
 </div>
