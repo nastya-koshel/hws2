815f1682f7c9003d42d5296d1ca8b5307da71dfc
Nastya Koshel <nastyakoshel68@gmail.com>
2026-07-10 18:19:21 +0300

    HW13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -7,7 +7,6 @@ import success200 from './images/200.svg'
 import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
-import {LinearProgress} from "@mui/material";
 
 /*
 * 1 - дописать функцию send
@@ -59,7 +58,6 @@ const HW13 = () => {
     return (
         <div id={'hw13'}>
             <div className={s2.hwTitle}>Homework #13</div>
-            {!!info ?  <LinearProgress color="primary" aria-label="Loading…" /> : null}
             <div className={s2.hw}>
                 <div className={s.buttonsContainer}>
                     <SuperButton
@@ -112,9 +110,9 @@ const HW13 = () => {
                         <div id={'hw13-text'} className={s.text}>
                             {text}
                         </div>
-                        {/*<div id={'hw13-info'} className={s.info}>*/}
-                        {/*    {info}*/}
-                        {/*</div>*/}
+                        <div id={'hw13-info'} className={s.info}>
+                            {info}
+                        </div>
                     </div>
                 </div>
             </div>
