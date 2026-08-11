0c306bcf2a4ffce6ce530b576b88f15bd1acf382
Nastya Koshel <nastyakoshel68@gmail.com>
2026-08-11 23:38:26 +0300

    HW14

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -32,6 +32,18 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
         onChangeText?.(value)
 
         if (onDebouncedChange) {
+            if (timerId) {
+                clearTimeout(timerId)
+                setTimerId(undefined)
+            }
+
+            const newTimerId = +setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500)
+
+            setTimerId(newTimerId)
+
+
             // делает студент
 
             // остановить предыдущий таймер
