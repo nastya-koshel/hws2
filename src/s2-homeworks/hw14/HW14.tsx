6319418634ca2ef0a946dd709409b64ff764a371
Nastya Koshel <nastyakoshel68@gmail.com>
2026-08-11 23:38:26 +0300

    HW14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -34,6 +34,11 @@ const HW14 = () => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
+                if (res) {
+                    setTechs(res.data.techs)
+                }
+                setLoading(false)
+
                 // делает студент
 
                 // сохранить пришедшие данные
@@ -44,6 +49,8 @@ const HW14 = () => {
 
     const onChangeText = (value: string) => {
         setFind(value)
+        setSearchParams({find: value})
+
         // делает студент
 
         // добавить/заменить значение в квери урла
